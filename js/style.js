48bece21f020d21af9aff64add3332aeab7442ca
Monica-Crespin <78183085+Monica-Crespin@users.noreply.github.com>
2021-04-15 15:43:38 -0600

    Alerta en página de contacto

--- js/style.js
+++ js/style.js
@@ -37,13 +37,43 @@ function ocultarMenu(){
 }
 
 //Validación del formulario
-function validarFormulario() {
-    const e = document.forms ['form'] ['name'] ['email'] ['messagetitle'] ['message'].value;
+$('#btnForm').click(function() {
+    const nombre = $('#nombre').val();
+    const correo = $('#correo').val();
+    const tituloMensaje = $('titulomensaje').val();
+    const mensaje = $('#mensaje');
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 9000,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
 
-    if (e === '') {
-        return false;
+    if (nombre == '' || correo == '' || tituloMensaje == '' || mensaje == '') {
+        Toast.fire({
+            icon: 'info',
+            title: 'Hay campos vacíos'
+        })
+    } else {
+        Swal.fire({
+            icon: 'success',
+            title: 'Tu mensaje se envió correctamente',
+            text: 'Recibirás tu respuesta en un plazo de 24 a 48 horas hábiles',
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            allowEnterKey: false,
+            stopKeydownPropagation: true,
+            showConfirmButton: false,
+            timer: 9000,
+            width: '55%',
+            height: '60%'
+        })
     }
-}
+})
 
 //Contador
 window.onscroll = function () {
